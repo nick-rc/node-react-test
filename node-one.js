9054e8fc9c881e3847a6e68d725de3df5dba1d4c
nick-rc <nick.rcraig@gmail.com>
2018-02-08 22:03:38 -0500

    Test Module

--- node-one.js
+++ node-one.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var dt   = require('./node-mod-1.js');
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': {'text/html'}});
-	res.end("Hello World!");
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write("The date an time are currently: " + dt.myDateTime());
+	res.end("\nHello World!");
 
 }).listen(8080);
